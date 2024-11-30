ade2aeae1102ac8a5d00108d2fb9c68e1a10a583
chaturvediit <117708146+chaturvediit@users.noreply.github.com>
2024-11-30 10:02:08 +0000

    add variavle

---
+++ 01_basek/01_variable.js
@@ -0,0 +1,12 @@
+const acountNo= 123456
+let Name= "Hello Mister Indresh Kuamr chaturvedi"
+var Email= "javascript.@gmail.come"
+ city = "Varanasi"
+ let subject
+
+
+//acountNo= 123451
+  Name= "Hello Mister"
+  Email= "javascri.@gmail.come"
+ city = "Delhi"
+console.table([acountNo,Name, Email,city,subject])
